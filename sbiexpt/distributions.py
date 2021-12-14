c035102b6223a1a8a994efc39fe84b1041d46e97
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2021-12-14 19:35:29 +0100

    Switch back to indent size 2 for python

--- sbiexpt/distributions.py
+++ sbiexpt/distributions.py
@@ -7,62 +7,62 @@ __all__ = ['get_swiss_roll', 'get_two_moons']
 
 
 def get_swiss_roll(sigma, resolution=1024):
-    """
-    Generate a TFP approximate distribution of the swiss roll dataset
+  """
+  Generate a TFP approximate distribution of the swiss roll dataset
 
-    Parameters
-    ----------
-    sigma: float
-        Spread of the 2 moons distribution.
-    resolution: int
-        Number of components in the gaussian mixture approximation of the
-        distribution (default: 1024)
+  Parameters
+  ----------
+  sigma: float
+    Spread of the 2 moons distribution.
+  resolution: int
+    Number of components in the gaussian mixture approximation of the
+    distribution (default: 1024)
 
-    Returns
-    -------
-    distribution: TFP distribution
-        Swiss roll distribution
-    """
-    n_samples = 2 * resolution
-    X, _ = make_swiss_roll(n_samples, noise=0)
-    coords = np.vstack([X[:, 0], X[:, 2]])
+  Returns
+  -------
+  distribution: TFP distribution
+    Swiss roll distribution
+  """
+  n_samples = 2 * resolution
+  X, _ = make_swiss_roll(n_samples, noise=0)
+  coords = np.vstack([X[:, 0], X[:, 2]])
 
-    distribution = tfd.MixtureSameFamily(
-        mixture_distribution=tfd.Categorical(probs=np.ones(2*resolution) / resolution / 2),
-        components_distribution=tfd.MultivariateNormalDiag(loc=coords.T, scale_identity_multiplier=sigma)
-    )
-    return distribution
+  distribution = tfd.MixtureSameFamily(
+      mixture_distribution=tfd.Categorical(probs=np.ones(2*resolution) / resolution / 2),
+      components_distribution=tfd.MultivariateNormalDiag(loc=coords.T, scale_identity_multiplier=sigma)
+  )
+  return distribution
 
 
 def get_two_moons(sigma, resolution=1024):
-    """
-    Generate a TFP approximate distribution of the two moons dataset
+  """
+  Generate a TFP approximate distribution of the two moons dataset
 
-    Parameters
-    ----------
-    sigma: float
-        Spread of the 2 moons distribution.
-    resolution: int
-        Number of components in the gaussian mixture approximation of the
-        distribution (default: 1024)
+  Parameters
+  ----------
+  sigma: float
+    Spread of the 2 moons distribution.
+  resolution: int
+    Number of components in the gaussian mixture approximation of the
+    distribution (default: 1024)
 
-    Returns
-    -------
-    distribution: TFP distribution
-        Two moons distribution
-    """
+  Returns
+  -------
+  distribution: TFP distribution
+    Two moons distribution
+  """
 
-    outer_circ_x = np.cos(np.linspace(0, np.pi, resolution))
-    outer_circ_y = np.sin(np.linspace(0, np.pi, resolution))
-    inner_circ_x = 1 - np.cos(np.linspace(0, np.pi, resolution))
-    inner_circ_y = 1 - np.sin(np.linspace(0, np.pi, resolution)) - .5
+  outer_circ_x = np.cos(np.linspace(0, np.pi, resolution))
+  outer_circ_y = np.sin(np.linspace(0, np.pi, resolution))
+  inner_circ_x = 1 - np.cos(np.linspace(0, np.pi, resolution))
+  inner_circ_y = 1 - np.sin(np.linspace(0, np.pi, resolution)) - .5
 
-    X = np.append(outer_circ_x, inner_circ_x)
-    Y = np.append(outer_circ_y, inner_circ_y)
-    coords = np.vstack([X, Y])
+  X = np.append(outer_circ_x, inner_circ_x)
+  Y = np.append(outer_circ_y, inner_circ_y)
+  coords = np.vstack([X, Y])
 
-    distribution = tfd.MixtureSameFamily(
-        mixture_distribution=tfd.Categorical(probs=np.ones(2*resolution) / resolution / 2),
-        components_distribution=tfd.MultivariateNormalDiag(loc=coords.T, scale_identity_multiplier=sigma)
-    )
-    return distribution
+  distribution = tfd.MixtureSameFamily(
+      mixture_distribution=tfd.Categorical(probs=np.ones(2*resolution) / resolution / 2),
+      components_distribution=tfd.MultivariateNormalDiag(loc=coords.T, scale_identity_multiplier=sigma)
+  )
+  return distribution
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='SBIExperiments',
-    version='0.0.1',
-    url='https://github.com/astrodeepnet/sbi_experiments',
-    author='Justine Zeghal and friends',
-    description='Package for numerical experiments of SBI tools',
-    packages=find_packages(),
-    install_requires=[
-        'numpy>=1.19.2',
-        'jax>=0.2.0',
-        'jaxlib>=0.1.74',
-        'tensorflow_probability>=0.14.1',
-        'scikit-learn>=0.21'
-    ],
+  name='SBIExperiments',
+  version='0.0.1',
+  url='https://github.com/astrodeepnet/sbi_experiments',
+  author='Justine Zeghal and friends',
+  description='Package for numerical experiments of SBI tools',
+  packages=find_packages(),
+  install_requires=[
+    'numpy>=1.19.2',
+    'jax>=0.2.0',
+    'jaxlib>=0.1.74',
+    'tensorflow_probability>=0.14.1',
+    'scikit-learn>=0.21'
+  ],
 )
