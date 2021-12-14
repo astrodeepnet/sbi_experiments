0e0a551bf45f4ea51c45456be266aeb6fd104114
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2021-12-14 16:37:58 +0100

    Add missing requirements

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(
     url='https://github.com/astrodeepnet/sbi_experiments',
     author='Justine Zeghal and friends',
     description='Package for numerical experiments of SBI tools',
-    packages=find_packages(),    
-    install_requires=['numpy >= 1.11.1', 'jax >= 0.2.0', 'tensorflow_probability >= 0.14.1'],
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.19.2',
+        'jax>=0.2.0',
+        'jaxlib>=0.1.74',
+        'tensorflow_probability>=0.14.1',
+        'scikit-learn>=0.21'
+    ],
 )
